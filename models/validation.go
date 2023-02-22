c507ca252f7bf438629719bd2d8d321dd5c09edd
Karol Moroz <karol@moroz.dev>
2023-02-22 01:47:58 +0100

    Also add URL validation

--- models/validation.go
+++ models/validation.go
@@ -1,10 +1,24 @@
 package models
 
 import (
+	"net/url"
 	"regexp"
 )
 
 var emailRegexp = regexp.MustCompile("^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$")
+
+func isValidURL(input string) bool {
+	u, err := url.Parse(input)
+	if err != nil {
+		return false
+	}
+
+	if u.Scheme != "http" && u.Scheme != "https" {
+		return false
+	}
+
+	return true
+}
 
 func (p *CreateCommentInput) Validate() []string {
 	errors := make([]string, 0)
@@ -19,6 +33,10 @@ func (p *CreateCommentInput) Validate() []string {
 
 	if !emailRegexp.Match([]byte(p.Email)) {
 		errors = append(errors, "Email is invalid")
+	}
+
+	if p.Website != nil && *p.Website != "" && !isValidURL(*p.Website) {
+		errors = append(errors, "Website URL must be a valid URL with http or https scheme")
 	}
 
 	return errors
